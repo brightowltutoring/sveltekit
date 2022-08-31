96316c40723e14c177f63190decdd0bf5b9abaca
brightowltutoring <106844324+brightowltutoring@users.noreply.github.com>
2022-08-30 23:06:19 -0400

    ...still trying to fix cloudflare bitching about 'Firebase/app' ...

--- .svelte-kit/cloudflare/_worker.js
+++ .svelte-kit/cloudflare/_worker.js
@@ -924,8 +924,8 @@ var init__ = __esm({
     init_layout();
     index = 0;
     component = async () => (await Promise.resolve().then(() => (init_layout_svelte(), layout_svelte_exports))).default;
-    file = "_app/immutable/components/pages/_layout.svelte-9fed5034.js";
-    imports = ["_app/immutable/components/pages/_layout.svelte-9fed5034.js", "_app/immutable/chunks/index-c60b019d.js", "_app/immutable/chunks/store-adcfcad0.js", "_app/immutable/chunks/index-71178bcf.js", "_app/immutable/chunks/singletons-b009a225.js", "_app/immutable/modules/pages/_layout.js-c3477997.js"];
+    file = "_app/immutable/components/pages/_layout.svelte-70237fb9.js";
+    imports = ["_app/immutable/components/pages/_layout.svelte-70237fb9.js", "_app/immutable/chunks/index-c60b019d.js", "_app/immutable/chunks/store-adcfcad0.js", "_app/immutable/chunks/index-71178bcf.js", "_app/immutable/chunks/singletons-259bf4f0.js", "_app/immutable/modules/pages/_layout.js-c3477997.js"];
     stylesheets = ["_app/immutable/assets/+layout-5845f018.css"];
   }
 });
@@ -1011,8 +1011,8 @@ var init__2 = __esm({
   ".svelte-kit/output/server/nodes/1.js"() {
     index2 = 1;
     component2 = async () => (await Promise.resolve().then(() => (init_error_svelte(), error_svelte_exports))).default;
-    file2 = "_app/immutable/components/error.svelte-39c1e1ea.js";
-    imports2 = ["_app/immutable/components/error.svelte-39c1e1ea.js", "_app/immutable/chunks/index-c60b019d.js", "_app/immutable/chunks/singletons-b009a225.js", "_app/immutable/chunks/index-71178bcf.js"];
+    file2 = "_app/immutable/components/error.svelte-8db72b4e.js";
+    imports2 = ["_app/immutable/components/error.svelte-8db72b4e.js", "_app/immutable/chunks/index-c60b019d.js", "_app/immutable/chunks/singletons-259bf4f0.js", "_app/immutable/chunks/index-71178bcf.js"];
     stylesheets2 = [];
   }
 });
@@ -9620,7 +9620,7 @@ var manifest = {
   assets: /* @__PURE__ */ new Set([".DS_Store", "login-bg-video-blurred.mp4"]),
   mimeTypes: { ".mp4": "video/mp4" },
   _: {
-    entry: { "file": "_app/immutable/start-0645dfef.js", "imports": ["_app/immutable/start-0645dfef.js", "_app/immutable/chunks/index-c60b019d.js", "_app/immutable/chunks/singletons-b009a225.js", "_app/immutable/chunks/index-71178bcf.js"], "stylesheets": [] },
+    entry: { "file": "_app/immutable/start-a1b437f3.js", "imports": ["_app/immutable/start-a1b437f3.js", "_app/immutable/chunks/index-c60b019d.js", "_app/immutable/chunks/singletons-259bf4f0.js", "_app/immutable/chunks/index-71178bcf.js"], "stylesheets": [] },
     nodes: [
       () => Promise.resolve().then(() => (init__(), __exports)),
       () => Promise.resolve().then(() => (init__2(), __exports2)),
--- .svelte-kit/output/client/_app/immutable/chunks/1-7a9d3811.js
+++
@@ -1,1 +0,0 @@
-import{default as e}from"../components/error.svelte-39c1e1ea.js";import"./index-c60b019d.js";import"./singletons-b009a225.js";import"./index-71178bcf.js";export{e as component};
