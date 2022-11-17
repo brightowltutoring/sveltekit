1b0e47bd20749a3413a320bea97f06da17a3af66
brightowltutoring <106844324+brightowltutoring@users.noreply.github.com>
2022-11-17 18:28:23 -0500

    experimenting again (works on npm run dev) with slot component version of intersection observer ... my only question is if this is less performant when needing to wrap a LOT of individvual elements (such as individual review stars)

--- src/lib/InView.svelte
+++ src/lib/InView.svelte
@@ -3,6 +3,7 @@
   import { onMount } from "svelte";
 
   export let once = false;
+  export let containee; // Without this prop specification the observation is on the slot container '<div bind:this={element}>'. When this prop IS specified the observation is done on the first child of <slot/> ... useful when modify a single wrapped element (e.g., adding an img attribute for lazy load image).
 
   export let action = () => {
     console.log("slots slots ssslots");
@@ -25,13 +26,30 @@
 
   let element;
 
+  // import { createEventDispatcher } from "svelte";
+  // let fire = createEventDispatcher();
+
+  // // when coupled with use: on an element below, this sends up the element node data ..in case I want to modify if I am observing the wrapping div 'element' or it's children
+  // function getObservedElement() {
+  //   fire("observedElement", { data: element });
+  // }
+  // export let observedElement;
+
   onMount(() => {
     const observer = new IntersectionObserver(handleIntersect, options);
 
-    observer.observe(element);
+    if (containee) {
+      observer.observe(element.children[0]);
+    } else {
+      observer.observe(element);
+    }
+    // if (observedElement) {
+    //   observer.observe(observedElement);
+    // } else observer.observe(element);
   });
 </script>
 
+<!-- <div bind:this={element} use:getObservedElement> -->
 <div bind:this={element}>
   <slot />
 </div>
--- src/lib/Reviews/Reviews.svelte
+++ src/lib/Reviews/Reviews.svelte
@@ -1,12 +1,10 @@
 <script>
   import ReviewCreator from "$lib/Reviews/reviewCreator.svx";
   import { reviews } from "$lib/Reviews/reviews.js";
-  import HydrateReviewStars from "$lib/Reviews/HydrateReviewStars.svelte";
+  // import HydrateReviewStars from "$lib/Reviews/HydrateReviewStars.svelte";
+  // <HydrateReviewStars /> adds star.webp images to ReviewCreator component (which contains img elements); removes opacity-0 from the css prop 'whenImageNotLoaded' ..i.e. this logic is a fallback when the image is not loaded in time
   let whenImageNotLoaded = "opacity-0 transition-opacity duration-500 ease-in";
 </script>
-
-<HydrateReviewStars />
-<!-- adds star.webp images to ReviewCreator component (which contains img elements); removes opacity-0 from the css prop 'whenImageNotLoaded' ..i.e. this logic is a fallback when the image is not loaded in time  -->
 
 <div class="scale-90 ">
   <!-- <div
