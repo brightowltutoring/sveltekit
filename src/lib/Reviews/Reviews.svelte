0a34cb24c9ec170f9d9e4a9a35091f05e7878399
brightowltutoring <106844324+brightowltutoring@users.noreply.github.com>
2022-11-17 23:40:14 -0500

    was finally able to restore the homework nav modal functionality ... i.e. closing modal doesn't cancel the started uploads. Also strangely .svx files don't like to be title-cased when deploying to github .... weird bug because it works locally on npm run dev OR npm run build

--- src/lib/Reviews/Reviews.svelte
+++ src/lib/Reviews/Reviews.svelte
@@ -1,5 +1,5 @@
 <script>
-  import ReviewCreator from "$lib/Reviews/ReviewCreator.svx";
+  import ReviewCreator from "$lib/Reviews/reviewCreator.svx";
   import { reviews } from "$lib/Reviews/reviews.js";
   // import HydrateReviewStars from "$lib/Reviews/HydrateReviewStars.svelte";
   // <HydrateReviewStars /> adds star.webp images to ReviewCreator component (which contains img elements); removes opacity-0 from the css prop 'whenImageNotLoaded' ..i.e. this logic is a fallback when the image is not loaded in time
