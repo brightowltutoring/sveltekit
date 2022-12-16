83cc210c2abef56263c5fb97408132dcf6a081a9
brightowltutoring <106844324+brightowltutoring@users.noreply.github.com>
2022-12-16 14:35:53 -0500

    looks like lazymounting review section on home page led to no perf gains. Currently testing out removing '{...76871props}' in LazyMount.svelte

--- src/lib/Wrappers/LazyMount.svelte
+++ src/lib/Wrappers/LazyMount.svelte
@@ -1,14 +1,15 @@
 <!-- dec14,530pm: havent yet figured out the passed bounded props  -->
 <script>
-  let Component;
   import InView from "$lib/Wrappers/InView.svelte";
 
   export let Import = async () => {};
-  // const getComponent = async () => (Component = await Import());
+  let Component;
+  const getComponent = async () => (Component = await Import());
 </script>
 
-<InView onview={async () => (Component = await Import())}>
+<InView onview={getComponent}>
   {#if Component}
-    <Component.default {...$$props} />
+    <Component.default />
+    <!-- <Component.default {...$$props} /> -->
   {/if}
 </InView>
