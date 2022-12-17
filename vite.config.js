64ae2d6620ce9b2a72beb9ffbbd895ad1dfc7c97
brightowltutoring <106844324+brightowltutoring@users.noreply.github.com>
2022-12-17 18:24:33 -0500

    1. removed minify option in vite config ..this was done before I used lazyMount on components, so want to compare if this has no effect (less config dependencies to track, the better). Also in vite config i forgot to mention in the past that I removed rollupOptions .. which was something i was tinkering with due to dynamic import of dropzone css was NOT working in an inview callback function.

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,15 @@ import { sveltekit } from "@sveltejs/kit/vite";
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [sveltekit()],
-  build: {
-    minify: true, // added dec 13,2022; not sure of perf gains
-    // rollupOptions: {
-    //   external: [
-    //     "/Users/brightowl/Documents/GitHub/sveltekit/node_modules/dropzone/dist/dropzone.css",
-    //   ],
-    //   //   external: ["/path/to/external/module.es.js"],
-    // },
-  },
+  // build: {
+  //   minify: true, // added dec 13,2022; not sure of perf gains
+  //   // rollupOptions: {
+  //   //   external: [
+  //   //     "/Users/brightowl/Documents/GitHub/sveltekit/node_modules/dropzone/dist/dropzone.css",
+  //   //   ],
+  //   //   //   external: ["/path/to/external/module.es.js"],
+  //   // },
+  // },
 };
 
 export default config;
