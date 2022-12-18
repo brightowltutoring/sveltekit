97684c46c7afae12df266e6005155bd0b5f5491e
brightowltutoring <106844324+brightowltutoring@users.noreply.github.com>
2022-12-18 03:14:12 -0500

    confirmed that lazyMounting the single component on plans page results in poorer ligthouse score... makes since as guessed in last deployment's comment

--- src/routes/plans/+page.svelte
+++ src/routes/plans/+page.svelte
@@ -1,13 +1,17 @@
 <script>
-  import LazyMount from "$lib/Wrappers/LazyMount.svelte";
-  // import PlansSection from "$lib/Plans/PlansSection.svelte";
+  import PlansSection from "$lib/Plans/PlansSection.svelte";
 </script>
 
-<LazyMount Import={() => import("$lib/Plans/PlansSection.svelte")} />
-<!-- <PlansSection /> -->
+<PlansSection />
 <!-- <PlansSection on:boop={(e) => console.log("😮", e.detail.message)} /> -->
 
 <!-- <PlansSection
   noTransition
   on:boop={(e) => console.log("😮", e.detail.message)}
 /> -->
+
+<!-- DEC 18 2022: for routes of a single component, makes sense to not lazy mount the component and allow the background prerendering to do it's thing  -->
+<!-- <script>
+  import LazyMount from "$lib/Wrappers/LazyMount.svelte";
+</script>
+<LazyMount Import={() => import("$lib/Plans/PlansSection.svelte")} /> -->
