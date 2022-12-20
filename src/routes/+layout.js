28a6c987250e67a1c703a96b3e7899532465b6ca
brightowltutoring <106844324+brightowltutoring@users.noreply.github.com>
2022-12-20 17:49:50 -0500

    setting minify in vite config...to see if there are noticeable differences on various routes under lightscore

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,4 +1,4 @@
-export const prerender = true;
+// export const prerender = true;
 // as of dec 2022 i'm commenting out 'prerender = true' here — i.e. defaulting back to SSR from SSG
 // JUSTIFICATION: google lighthouse scores are the same (perfect on home page). This is due to my implementing event-based dynamic importing everywhere via InView/LazyMount wrapper components.
 
