08bb82b32c5c7db0269d3ecf19474f06df16b489
brightowltutoring <106844324+brightowltutoring@users.noreply.github.com>
2022-12-21 13:05:36 -0500

    retrying facepalm.gif in error.svelte

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,6 +1,4 @@
 <script>
-  import InView from "$lib/Wrappers/InView.svelte";
-  let src = "";
   // import { page } from "$app/stores";$page.status
 </script>
 
@@ -10,8 +8,10 @@
   <!-- <div class="animate-bounce text-4xl">🙈</div> -->
 </div>
 
-<InView once onview={() => (src = "/facepalm.gif")}>
-  <div class="flex justify-center">
-    <img src="" alt="face palm star trek gif" class="w-1/3 min-w-[250px]" />
-  </div>
-</InView>
+<div class="flex justify-center">
+  <img
+    src="/facepalm.gif"
+    alt="face palm star trek gif"
+    class="w-1/3 min-w-[250px]"
+  />
+</div>
