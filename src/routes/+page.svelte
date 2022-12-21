7fa80208dabb04dbe6952756e56cc47c1a08582b
brightowltutoring <106844324+brightowltutoring@users.noreply.github.com>
2022-12-21 17:32:30 -0500

    testing out zsh script function, which previously worked to deploy this app to github

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,7 +16,6 @@
   $: gradientTextColor = `text-transparent bg-clip-text bg-gradient-to-tr ${
     $isDarkMode ? "from-red-300 via-white to-white" : "from-indigo-600 to-black"
   }`;
-  console.log("🎤 drop");
 </script>
 
 <video
