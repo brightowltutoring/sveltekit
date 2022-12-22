f05ba0992bedf6dd3009aca87e0c19d1ff50b4d2
brightowltutoring <106844324+brightowltutoring@users.noreply.github.com>
2022-12-22 18:36:10 -0500

    after restoring .env file and npm run build working ...im still not getting it to work with cloudflare deployment ... so now im commenting out env references in gitignore

--- .svelte-kit/output/server/nodes/11.js
+++ .svelte-kit/output/server/nodes/11.js
@@ -4,6 +4,6 @@ export const index = 11;
 export const component = async () => (await import('../entries/pages/physics/_page.svelte.js')).default;
 export const file = '_app/immutable/components/pages/physics/_page.svelte-123e9f6b.js';
 export { universal };
-export const imports = ["_app/immutable/components/pages/physics/_page.svelte-123e9f6b.js","_app/immutable/chunks/index-06adba90.js","_app/immutable/chunks/Kajax-a6b1bd4f.js","_app/immutable/chunks/katex-589a4957.js","_app/immutable/chunks/store-e723d68b.js","_app/immutable/chunks/index-bc04d146.js","_app/immutable/modules/pages/physics/_page.js-7dc16dc1.js","_app/immutable/chunks/_page-37f6be6a.js"];
+export const imports = ["_app/immutable/components/pages/physics/_page.svelte-123e9f6b.js","_app/immutable/chunks/index-06adba90.js","_app/immutable/chunks/Kajax-a6b1bd4f.js","_app/immutable/chunks/katex-589a4957.js","_app/immutable/chunks/store-e723d68b.js","_app/immutable/chunks/index-bc04d146.js","_app/immutable/modules/pages/physics/_page.js-7dc16dc1.js","_app/immutable/chunks/_page-621564aa.js"];
 export const stylesheets = ["_app/immutable/assets/_page-fcf51fa8.css"];
 export const fonts = [];
