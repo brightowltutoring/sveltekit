cf7548b32a8a751557ddf371b3c183334282b2a6
brightowltutoring <106844324+brightowltutoring@users.noreply.github.com>
2022-12-30 18:08:48 -0500

    just realized haven't pushed updates to main app; have bee testing on vercel test repository

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,7 +17,7 @@
     $isDarkMode ? "from-red-300 via-white to-white" : "from-indigo-600 to-black"
   }`;
 
-  console.log("cloudflare deploy, boy");
+  // console.log("cloudflare deploy, boy");
 </script>
 
 <video
