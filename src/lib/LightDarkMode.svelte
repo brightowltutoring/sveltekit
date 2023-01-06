74218dcf51a2ecf7782c1d35cebd6fa47937783e
brightowltutoring <106844324+brightowltutoring@users.noreply.github.com>
2023-01-06 04:26:09 -0500

    just realized that since im using <svelte:head> ... i dont need to place it in +layout .svelte; now all darkmode logic is inside lightdarkmode.svelte .... only wish i could define 'initialTheme()' once in this component and use in both the regular script tag and the script tag inside <svelte:head>

--- src/lib/LightDarkMode.svelte
+++ src/lib/LightDarkMode.svelte
@@ -26,18 +26,39 @@
   }
 </script>
 
-{#key $isDarkMode}
-  <!-- Currently using nested 'divs' to compose two svelte transitions. This can be done with a custom function as well (TODO:) such as on: https://svelte.dev/repl/f5c42c6dc6774f29ad9350cd2dc2d299?version=3.38.3 -->
-  <div in:slide={{ duration: 600, easing: quintOut }}>
-    <button
-      on:click={toggleDM}
-      in:scale={{ duration: 1000, easing: elasticOut }}
-    >
-      {#if $isDarkMode}
-        <IconSun />
-      {:else}
-        <IconMoon />
-      {/if}
-    </button>
-  </div>
-{/key}
+<main>
+  {#key $isDarkMode}
+    <!-- Currently using nested 'divs' to compose two svelte transitions. This can be done with a custom function as well (TODO:) such as on: https://svelte.dev/repl/f5c42c6dc6774f29ad9350cd2dc2d299?version=3.38.3 -->
+    <div in:slide={{ duration: 600, easing: quintOut }}>
+      <button
+        on:click={toggleDM}
+        in:scale={{ duration: 1000, easing: elasticOut }}
+      >
+        {#if $isDarkMode}
+          <IconSun />
+        {:else}
+          <IconMoon />
+        {/if}
+      </button>
+    </div>
+  {/key}
+</main>
+
+<svelte:head>
+  <script>
+    if (initialTheme() === "dark") {
+      document.documentElement.classList.add("dark-mode");
+    }
+
+    function initialTheme() {
+      if (sessionStorage.getItem("isDarkModeLS") === "true") return "dark";
+      if (sessionStorage.getItem("isDarkModeLS") === "false") return "light";
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches)
+        return "dark";
+      return "light";
+    }
+  </script>
+  <!-- Using this head script — which fires before the body is loaded — achieves darkmode WITHOUT an initial flash of content DESPITE retrieving darkmode information from the client (sessionStorage & window.matchMedia) -->
+  <!-- 
+      ... So why do people insist on complex server-side methods involving framework-specific code AND cookies API ?? This vanilla-ish ('ish' because other half in lightdarkmode.svelte) approach also happens to be 1) less code, 2) simpler to read.   Cookies offer a custom lifetime, however isn't the desired approach for this boolean theme (dark/light) to 1) initialize with device settings, 2) update with session settings? -->
+</svelte:head>
