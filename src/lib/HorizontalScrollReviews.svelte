282d608783864d1f713f8abe799e850c1222c38e
brightowltutoring <106844324+brightowltutoring@users.noreply.github.com>
2023-02-22 21:03:53 -0500

    tweak last deployment

--- src/lib/HorizontalScrollReviews.svelte
+++ src/lib/HorizontalScrollReviews.svelte
@@ -6,15 +6,13 @@
 </script>
 
 <!-- example taken from: https://tailwindcomponents.com/component/horizontal-scroll-card-components -->
-<section class="p-auto m-auto my-10 flex flex-col  py-10">
-	<div class="hide-scroll-bar flex overflow-x-scroll ">
+<section class="p-auto m-auto my-10 flex   flex-col ">
+	<div class="hide-scroll-bar flex overflow-x-scroll  ">
 		<div class="mx-10 flex flex-nowrap md:mx-20 lg:mx-40 ">
 			<!-- {#each Array(30) as _, i} -->
 			{#each reviews as { name, title, date, body }}
-				<div class="inline-block px-3">
-					<div
-						class="hide-scroll-bar  card  h-[600px] w-96 max-w-sm overflow-hidden overflow-y-scroll rounded-lg p-10 shadow-md transition-shadow duration-300 ease-in-out  hover:shadow-xl"
-					>
+				<div class="inline-block  px-3">
+					<div class="card">
 						<ReviewCreator {title} {name} {date}>
 							{body}
 						</ReviewCreator>
@@ -35,7 +33,7 @@
 	}
 
 	.card {
-		@apply bg-white;
+		@apply hide-scroll-bar h-[500px]  w-96 max-w-sm overflow-hidden overflow-y-scroll rounded-xl bg-white p-5 shadow-md transition-shadow duration-300 ease-in-out  hover:shadow-xl;
 	}
 
 	:global(html.dark-mode) .card {
