12640dc437b7fb4c5a21f847e8b4905bbee9fe05
brightowltutoring <106844324+brightowltutoring@users.noreply.github.com>
2023-02-25 18:44:38 -0500

    HorizontalScrollReviews.svelte: decided to move @const functions under the each block to a single function defintion inside the script tag .... wasnt a fan of creating 2N functions  ...also component is small enough to not have to rely on @const logic

--- src/routes/reviews/HorizontalScrollReviews.svelte
+++ src/routes/reviews/HorizontalScrollReviews.svelte
@@ -1,8 +1,18 @@
-<script>
+<script lang="ts">
 	import { reviews } from './reviews';
 
 	// @ts-ignore
 	import ReviewCreator from './ReviewCreator.svelte';
+
+	function horizScroll(index: number, to = 'right') {
+		let target_index = to === 'left' ? index - 1 : index + 1;
+
+		document
+			.getElementById(`card_${target_index % reviews.length}`)
+			?.scrollIntoView({ behavior: 'smooth', block: 'center' });
+	}
+
+	// previously was defining two functions per index under svelte each block using @const .. however this component is small enough to keep function definitions in script tag ... also not a fan of creating 2N functions
 </script>
 
 <!-- example taken from: https://tailwindcomponents.com/component/horizontal-scroll-card-components -->
@@ -13,17 +23,6 @@
 	<div class="mx-10 flex gap-x-5 md:mx-20 lg:mx-40">
 		<!-- {#each Array(30) as _, i} -->
 		{#each reviews as { name, title, date, body }, index}
-			{@const scrollToRightCard = () => {
-				document
-					.getElementById(`card_${(index + 1) % reviews.length}`)
-					?.scrollIntoView({ behavior: 'smooth', block: 'center' });
-			}}
-			<!-- if (index == 0) index = reviews.length; -->
-			{@const scrollToLeftCard = () => {
-				document
-					.getElementById(`card_${(index - 1) % reviews.length}`)
-					?.scrollIntoView({ behavior: 'smooth', block: 'center' });
-			}}
 			<!-- hideScrollBar   -->
 			<!-- w-96 -->
 			<div
@@ -32,12 +31,12 @@
 			>
 				<button
 					aria-label="right horizontal scroll to next review card"
-					on:click={scrollToRightCard}
+					on:click={() => horizScroll(index, 'right')}
 					class="absolute right-0 top-0 z-10 h-full w-16 rounded-full"
 				/>
 				<button
 					aria-label="left horizontal scroll to next review card"
-					on:click={scrollToLeftCard}
+					on:click={() => horizScroll(index, 'left')}
 					class="absolute left-0  top-0 z-10 h-full w-16 rounded-full"
 				/>
 				<ReviewCreator {title} {name} {date}>
