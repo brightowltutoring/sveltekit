a9d1b243ddd41e695a11252b97fac2b1c970716a
brightowltutoring <106844324+brightowltutoring@users.noreply.github.com>
2023-03-20 18:42:56 -0400

    returning GoogleLogin.ts as before and placing signinWithRedirect sample code inside GoogleLogin-EXPERIMENTAL.ts ...to continue experimenting at a later date

---
+++ src/routes/login/GoogleLogin-EXPERIMENTAL.ts
@@ -0,0 +1,71 @@
+import { auth } from './firebase';
+
+import { lessThan768 } from '$lib/store';
+import { get } from 'svelte/store';
+
+// TODOnov29.2022 noticed that ' ' logic did not sign me in ... furthermore 'signInWithPopup' worked perfectly on both the PWA and on the mobile version of the website ... It appears firebase has consolidated the two??
+// UPDATE:  dec17,2022: 'https://firebase.google.com/docs/auth/web/redirect-best-practices' explains how and why 'signInWithRedirect' fails .. seems like just using 'signInWithPopup' is easiest option
+export async function GoogleLogin() {
+	// const auth = await import("$lib/Login/firebase");
+
+	const {
+		GoogleAuthProvider,
+		// setPersistence,
+		// browserSessionPersistence,
+		// inMemoryPersistence,
+		signInWithRedirect,
+		browserPopupRedirectResolver
+	} = await import('firebase/auth');
+
+	const provider = new GoogleAuthProvider();
+
+	// if (isPWA()) {
+
+	if (true) {
+		// if (get(lessThan768)) {
+		// const { signInWithRedirect } = await import('firebase/auth');
+		await signInWithRedirect(auth, provider, browserPopupRedirectResolver)
+			// signInWithRedirect(auth, provider)
+			.then((result) => {
+				// This gives you a Google Access Token. You can use it to access the Google API.
+				const credential = GoogleAuthProvider.credentialFromResult(result);
+				// const token = credential!.accessToken;
+				// The signed-in user info.
+				// const user = result.user;
+				alert('signInWithRedirect worked!');
+			})
+			.catch((error) => {
+				alert('there was an error with const signInWithRedirect');
+				console.log('signInWithRedirect error', error);
+				// Handle Errors here.
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				// The email of the user's account used.
+				const email = error.customData.email;
+				// The AuthCredential type that was used.
+				const credential = GoogleAuthProvider.credentialFromError(error);
+			});
+	} else {
+		const { signInWithPopup } = await import('firebase/auth');
+
+		// setPersistence(auth, browserSessionPersistence).then(() => {
+		signInWithPopup(auth, provider, browserPopupRedirectResolver)
+			.then((result) => {
+				// This gives you a Google Access Token. You can use it to access the Google API.
+				const credential = GoogleAuthProvider.credentialFromResult(result);
+				const token = credential!.accessToken;
+				// The signed-in user info.
+				const user = result.user;
+			})
+			.catch((error) => {
+				// Handle Errors here.
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				// The email of the user's account used.
+				const email = error.customData.email;
+				// The AuthCredential type that was used.
+				const credential = GoogleAuthProvider.credentialFromError(error);
+			});
+		// }); // set persistence block
+	}
+}
