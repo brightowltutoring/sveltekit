06238cc7c87cbbcd4cb02f844c62f76a25999bff
brightowltutoring <106844324+brightowltutoring@users.noreply.github.com>
2023-03-31 00:01:58 -0400

    trying alternate upload method in submitHomework form action function ... which uses the fetch API; oddly the old way and this way work with both pnpm run dev and pnpm run build and preview...

--- src/routes/homework/submitHomework.ts
+++ src/routes/homework/submitHomework.ts
@@ -1,20 +1,46 @@
 import type { RequestEvent } from '@sveltejs/kit';
-// import { PUBLIC_UPLOAD_ENDPOINT } from '$env/static/public';
+import { PUBLIC_UPLOAD_ENDPOINT } from '$env/static/public';
 
 export async function submitHomework(event: RequestEvent) {
 	const formData = await event.request.formData();
 
 	const files = formData.getAll('file') as File[];
-	// console.log('files', files);
 
-	// handle these with promise all
-	for (let file of files) {
+	// OLD METHOD: fetch is called "in series", so code is slower
+	// TODO: UPDATE: cannot tell if it is actually slower
+	// for (let file of files) {
+	// 	let data = new FormData();
+	// 	data.append('file', file, file.name);
+
+	// 	// handle these with promise all
+	// 	// fetch('https://us-central1-thinksolve-app.cloudfunctions.net/postToGoogleDriveGCF/formidable', {
+	// 	fetch(PUBLIC_UPLOAD_ENDPOINT, {
+	// 		method: 'POST',
+	// 		body: data
+	// 	});
+	// }
+
+	// NEW METHOD:  all fetches done "in parallel" with Promise.all logic
+	uploadInParallel().catch((e) => console.log('This upload failed', e));
+
+	async function uploadInParallel() {
+		const uploadsTodo = files.map((file) => {
+			try {
+				uploadSingle(file);
+			} catch (error) {
+				error;
+				console.log('error', error);
+			}
+		});
+
+		return await Promise.all(uploadsTodo);
+	}
+
+	async function uploadSingle(file: File) {
 		let data = new FormData();
-		data.append('file', file);
-
-		// handle these with promise all
+		data.append('file', file, file.name);
+		// fetch(PUBLIC_UPLOAD_ENDPOINT, {
 		fetch('https://us-central1-thinksolve-app.cloudfunctions.net/postToGoogleDriveGCF/formidable', {
-			// fetch(PUBLIC_UPLOAD_ENDPOINT, {
 			method: 'POST',
 			body: data
 		});
