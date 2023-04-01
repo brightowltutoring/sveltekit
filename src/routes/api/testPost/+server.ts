7847db722aff3a2e76684c7ab5ca0019f0ef493e
brightowltutoring <106844324+brightowltutoring@users.noreply.github.com>
2023-04-01 19:44:42 -0400

    redeploying code after pnpm run bp

--- src/routes/api/testPost/+server.ts
+++ src/routes/api/testPost/+server.ts
@@ -7,23 +7,25 @@ export async function POST(event: RequestEvent) {
 	let fileNames = files.map((file) => file.name);
 	const fileNamesOneString2 = fileNames.join(', ');
 
+	console.log(fileNamesOneString2);
+
 	const blob = fileNamesOneString2;
 	return new Response(blob, { status: 303 });
-
-	// console.log('files', files);
-
-	// method 1
-	// let fileNamesOneString = '';
-	// const filesLength = files.length;
-	// files.forEach((file, index) => {
-	// 	if (index != filesLength - 1) {
-	// 		fileNamesOneString += `${file.name},`;
-	// 	} else {
-	// 		fileNamesOneString += `${file.name}`;
-	// 	}
-	// });
-
-	// const blob = JSON.stringify(fileNamesOneString);
-
-	// return new Response(blob, { status: 303 });
 }
+// console.log('files', files);
+
+// method 1
+// let fileNamesOneString = '';
+// const filesLength = files.length;
+// files.forEach((file, index) => {
+// 	if (index != filesLength - 1) {
+// 		fileNamesOneString += `${file.name},`;
+// 	} else {
+// 		fileNamesOneString += `${file.name}`;
+// 	}
+// });
+
+// const blob = JSON.stringify(fileNamesOneString);
+
+// return new Response(blob, { status: 303 });
+// }
