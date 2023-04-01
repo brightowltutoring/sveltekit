fe55402f690b053ed7ee12054fdaadf76326542f
brightowltutoring <106844324+brightowltutoring@users.noreply.github.com>
2023-04-01 19:13:29 -0400

    retesting previous code without JSON.string method wrapping the string

--- src/routes/api/testPost/+server.ts
+++ src/routes/api/testPost/+server.ts
@@ -1,29 +1,29 @@
 import type { RequestEvent } from '@sveltejs/kit';
 
 export async function POST(event: RequestEvent) {
-	const formData = await event.request.formData(); // parse the request body as FormData
+	const formData = await event.request.formData();
 	let files = formData.getAll('file') as File[];
 
-	console.log('files', files);
+	let fileNames = files.map((file) => file.name);
+	const fileNamesOneString2 = fileNames.join(', ');
 
-	let fileNamesOneString = '';
+	const blob = fileNamesOneString2;
+	return new Response(blob, { status: 303 });
+
+	// console.log('files', files);
 
 	// method 1
-	const filesLength = files.length;
-	files.forEach((file, index) => {
-		if (index != filesLength - 1) {
-			fileNamesOneString += `${file.name},`;
-		} else {
-			fileNamesOneString += `${file.name}`;
-		}
-	});
+	// let fileNamesOneString = '';
+	// const filesLength = files.length;
+	// files.forEach((file, index) => {
+	// 	if (index != filesLength - 1) {
+	// 		fileNamesOneString += `${file.name},`;
+	// 	} else {
+	// 		fileNamesOneString += `${file.name}`;
+	// 	}
+	// });
 
-	// method 2
-	let fileNames = files.map((file) => file.name);
-	const fileNamesOneString2 = fileNames.join();
+	// const blob = JSON.stringify(fileNamesOneString);
 
-	const blob = JSON.stringify(fileNamesOneString);
-	// const blob = JSON.stringify(fileNamesOneString2);
-
-	return new Response(blob, { status: 303 });
+	// return new Response(blob, { status: 303 });
 }
