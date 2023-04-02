dca02b1215ca2155523942fc9bafb2441c4ea063
brightowltutoring <106844324+brightowltutoring@users.noreply.github.com>
2023-04-01 20:02:05 -0400

    changing adapter to auto to see if i can deploy to BOTH cloudflare and vercel for the same code

--- svelte.config.js
+++ svelte.config.js
@@ -2,18 +2,18 @@ import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 import preprocess from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import adapter from '@sveltejs/adapter-cloudflare';
-// import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	vitePlugin: { experimental: { inspector: true } },
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
 	kit: {
-		adapter: adapter(),
-		csrf: {
-			checkOrigin: false
-		}
+		adapter: adapter()
+		// csrf: {
+		// 	checkOrigin: false
+		// }
 	},
 	preprocess: [vitePreprocess(), preprocess({ postcss: true }), mdsvex(mdsvexConfig)]
 };
