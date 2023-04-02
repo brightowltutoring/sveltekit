a1ab06f84f67be4375f523be15614b179266411d
brightowltutoring <106844324+brightowltutoring@users.noreply.github.com>
2023-04-02 18:22:52 -0400

    changing adapter to cloudflare, from auto

--- svelte.config.js
+++ svelte.config.js
@@ -2,8 +2,8 @@ import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 import preprocess from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-// import adapter from '@sveltejs/adapter-cloudflare';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
+// import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
