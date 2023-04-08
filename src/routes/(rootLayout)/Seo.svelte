ccd17a3e1867d28263c032f55d984670e44da278
brightowltutoring <106844324+brightowltutoring@users.noreply.github.com>
2023-04-08 16:49:46 -0400

    getting weird lighthouse errors with this new change; so using seo.svelte the odl way

--- src/routes/(rootLayout)/Seo.svelte
+++ src/routes/(rootLayout)/Seo.svelte
@@ -1,25 +1,46 @@
 <script lang="ts">
-	import { routes } from '$lib/store';
+	import { routes, routes2 } from '$lib/store';
 	import { page } from '$app/stores';
 
 	$: routeId = $page.route.id;
 
-	let routes$ = Object.values($routes);
+	// let routes$ = Object.values($routes);
 </script>
 
-<svelte:head>
+<!-- <svelte:head>
 	{#if $page.status === 404}
 		<title>Oops 💩</title>
 	{:else}
 		{#each routes$ as key, i}
 			{@const { title, meta, href } = key}
 
-			<!--TODO: even with this code I have to separate home route from rest ... why?? -->
 			{#if i === 0 && routeId === '/'}
 				<title>{routes$[0].title}</title>
 
 				{@html routes$[0].meta}
 			{/if}
+
+			{#if routeId?.includes(href)}
+				<title>{title}</title>
+
+				{#if meta}
+					{@html meta}
+				{/if}
+			{/if}
+		{/each}
+	{/if}
+</svelte:head> -->
+
+<svelte:head>
+	{#if $page.status === 404}
+		<title>Oops 💩</title>
+	{:else if routeId?.slice(1) === ''}
+		<title>{$routes2[0].title}</title>
+
+		{@html $routes2[0].meta}
+	{:else}
+		{#each $routes2 as key}
+			{@const { title, meta, href } = key}
 
 			{#if routeId?.includes(href)}
 				<title>{title}</title>
