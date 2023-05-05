f3575ead2ee618b43408475af42df19b02d996b1
brightowltutoring <106844324+brightowltutoring@users.noreply.github.com>
2023-05-04 23:18:36 -0400

    not sure whats going on anymore ... in development things are working as expected but not when deployed .... at the moment im setting security equal to true when setting the cookie on the client side

--- src/routes/login/LoginCard.svelte
+++ src/routes/login/LoginCard.svelte
@@ -34,10 +34,10 @@
 	}
 
 	//TODO: for some reason $isLoggedIn, initialized via cookie, is falsy on safari ... even it previously set to true ... According to 'https://github.com/sveltejs/kit/issues/6632' the fix involves setting 'secure' to false when setting the cookie
-	$: if ($isLoggedIn || (!$isLoggedIn && $showLoginModal))
-		/* onMount(onMountFirebase); */ onMount(async () => await onMountFirebase());
+	// $: if ($isLoggedIn || (!$isLoggedIn && $showLoginModal))
+	// 	/* onMount(onMountFirebase); */ onMount(async () => await onMountFirebase());
 
-	// onMount(async () => await onMountFirebase());
+	onMount(async () => await onMountFirebase());
 
 	async function onMountFirebase() {
 		console.log('check check');
@@ -72,7 +72,8 @@
 			if (user) {
 				$isLoggedIn = true;
 
-				cookeh.set('haventLoggedOut', $isLoggedIn, { secure: !$isSafari });
+				cookeh.set('haventLoggedOut', $isLoggedIn, { secure: true });
+				// cookeh.set('haventLoggedOut', $isLoggedIn, { secure: !$isSafari });
 				// cookeh.set('haventLoggedOut', $isLoggedIn);
 
 				loggedInEmail = user.email;
