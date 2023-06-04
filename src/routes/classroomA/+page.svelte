a2ea01ebaf6613bc290c27f1fc28834b72c9620b
brightowltutoring <106844324+brightowltutoring@users.noreply.github.com>
2023-06-04 03:48:02 -0400

    adding jitsi head script on '/classroomA' route specfically, as direct navigation to this route breaks jitsi

--- src/routes/classroomA/+page.svelte
+++ src/routes/classroomA/+page.svelte
@@ -3,3 +3,9 @@
 </script>
 
 <JitsiUser isAdmin={true} />
+
+<svelte:head>
+	<script
+		src="https://8x8.vc/vpaas-magic-cookie-0c93f8c4b4d6403cbf9746e035d5660e/external_api.js"
+	></script>
+</svelte:head>
