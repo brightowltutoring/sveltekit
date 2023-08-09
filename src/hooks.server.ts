51472fbd1eb7266162cf7e6ee50316e0f93bf05d
brightowltutoring <106844324+brightowltutoring@users.noreply.github.com>
2023-08-09 19:57:54 -0400

    test2

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,23 +1,23 @@
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
-import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit'
+// import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
+// import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit'
 
 export async function handle({ event, resolve }) {
 
     event.locals.haventLoggedOut = Boolean(event.cookies.get('haventLoggedOut'));
     
-    event.locals.supabase = createSupabaseServerClient({
-      supabaseUrl: PUBLIC_SUPABASE_URL,
-      supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
-      event,
-    })
+    // event.locals.supabase = createSupabaseServerClient({
+    //   supabaseUrl: PUBLIC_SUPABASE_URL,
+    //   supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
+    //   event,
+    // })
 
-   	event.locals.getSession = async () => await event.locals.supabase.auth.getSession().data.session
+   	// event.locals.getSession = async () => await event.locals.supabase.auth.getSession().data.session
 
-    // let response = await resolve(event);
-    let response = await resolve(event, {
-      filterSerializedResponseHeaders(name) {
-        return name === 'content-range'
-      },
-    })
+    let response = await resolve(event);
+    // let response = await resolve(event, {
+    //   filterSerializedResponseHeaders(name) {
+    //     return name === 'content-range'
+    //   },
+    // })
    return response;
 }
